49ea622d14dcf43b711f67af6d135f11d9b032e4
wtotw <s135061@gmail.com>
2022-01-16 00:37:58 +0900

    動作確認

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue';
+import VText from '@/components/forms/VText.vue';
+import { ref } from 'vue';
+
+const text = ref('テキスト');
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <VText v-model="text" />
+  <p>{{ text }}</p>
 </template>
 
 <style>
