0952bdda93a90cd2adff799f89fbadfe01e9f3cb
wtotw <s135061@gmail.com>
2022-01-15 20:06:38 +0900

    設定を最新化

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    '@': path.join(__dirname, 'src')
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    },
   },
   plugins: [vue()]
 })
