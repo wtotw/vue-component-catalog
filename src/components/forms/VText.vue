6c3a18ad36ce414e252a47e9e0e30d15c980fab8
wtotw <s135061@gmail.com>
2022-01-15 19:45:47 +0900

    VText追加

---
+++ src/components/forms/VText.vue
@@ -0,0 +1,25 @@
+<template>
+  <input type="text" v-model="model">
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+
+export default defineComponent({
+  model: {
+    prop: 'value',
+    event: 'input',
+  },
+  props: {
+    value: String,
+  },
+  setup(props, context) {
+    const model = computed({
+      get: () => props.value,
+      set: (value: string | undefined) => { context.emit('input', value) },
+    })
+
+    return { model }
+  },
+})
+</script>
