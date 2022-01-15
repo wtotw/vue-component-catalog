f35d5683f16a940bc9e4eeda87bc8c32c0ddafef
wtotw <s135061@gmail.com>
2022-01-15 19:45:32 +0900

    設定追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    '@': path.join(__dirname, 'src')
+  },
   plugins: [vue()]
 })
