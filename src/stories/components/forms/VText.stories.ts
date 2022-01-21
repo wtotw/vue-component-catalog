5adced0a85d2ac23cd09de291607bdda0ec63136
wtotw <s135061@gmail.com>
2022-01-22 00:05:25 +0900

    VText.storiesを追加

---
+++ src/stories/components/forms/VText.stories.ts
@@ -0,0 +1,37 @@
+import { ArgTypes, Meta, Story } from '@storybook/vue3';
+import VText from '@/components/forms/VText.vue';
+import { ref } from 'vue';
+
+export default {
+  title: 'forms/VText',
+  component: VText,
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['text', 'password'],
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+} as Meta;
+
+const Template: Story = (args: ArgTypes) => ({
+  components: { VText },
+  setup() {
+    const text = ref('');
+
+    return { args, text };
+  },
+  template: `
+  <VText v-bind="args" v-model="text" />
+  <p>{{ text }}</p>
+  `,
+});
+
+export const Basic: Story = Template.bind({});
+Basic.args = {};
