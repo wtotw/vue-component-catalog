5670751016147dd3f7b7e9f2a69f85037ec0f701
wtotw <s135061@gmail.com>
2022-01-23 01:56:08 +0900

    linter適用

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,11 @@ const text = ref('テキスト');
 
 <style>
 #app {
+  margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
