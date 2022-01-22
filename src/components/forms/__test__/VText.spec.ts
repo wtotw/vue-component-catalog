6e31c32d436626ccd90862e2c0a23e281479d777
wtotw <s135061@gmail.com>
2022-01-23 01:49:34 +0900

    テスト修正

--- src/components/forms/__test__/VText.spec.ts
+++ src/components/forms/__test__/VText.spec.ts
@@ -11,4 +11,35 @@ describe('VText', () => {
   test('Vueインスタンスであること', () => {
     expect(wrapper.vm).toBeTruthy();
   });
+
+  test('inputタグであること', () => {
+    const input = wrapper.find('.v-text');
+    expect(input.element.tagName).toBe('INPUT');
+  });
+
+  test('typeが変更できること', async () => {
+    const input = wrapper.find('.v-text');
+    expect(input.attributes().type).toContain('text');
+
+    await wrapper.setProps({ type: 'number' });
+    expect(input.attributes().type).toContain('number');
+
+    await wrapper.setProps({ type: 'password' });
+    expect(input.attributes().type).toContain('password');
+  });
+
+  test('属性が変更できること', async () => {
+    const input = wrapper.find('.v-text');
+    expect(input.attributes().disabled).toBeUndefined();
+
+    await wrapper.setProps({ disabled: true });
+    expect(input.attributes().disabled).toBe('');
+  });
+
+  // FIXME: setup関数内のcomputedをテスト
+  test('値が変更できること', async () => {
+    // expect(wrapper.vm.$data).toBe('');
+    // await wrapper.setProps({ modelValue: 'テキスト' });
+    // expect(wrapper.vm.value).toBe('テキスト');
+  });
 });
